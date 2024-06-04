6c90f74d40d7799fc31bb94b3aaface6d1a8a06c
MohammadNassar4 <138707772+MohammadNassar4@users.noreply.github.com>
2024-06-04 08:11:54 +0300

    Created the createGrid function that creates a grid of the size that passed to it.

---
+++ script.js
@@ -0,0 +1,13 @@
+let container = document.querySelector(".container");
+let sketch = document.querySelector(".sketch");
+// A function that creates a grid of size of the variable size
+function createGrid(size) {
+    sketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketch.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for(let i = 0; i < size * size; i++) {
+        let div = document.createElement('div');
+        div.classList.add("grid-item");
+        sketch.appendChild(div);
+    }
+}
+
