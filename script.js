3b999147a204b909f3d4aecdf0a63d865503db42
MohammadNassar4 <138707772+MohammadNassar4@users.noreply.github.com>
2024-06-04 09:00:22 +0300

    Created the reset button that takes the size of the sketch from the user and create a new one after deleting the old one.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let container = document.querySelector(".container");
 let sketch = document.querySelector(".sketch");
 // A function that creates a grid of size of the variable size
 function createGrid(size) {
+    sketch.innerHTML = "";
     sketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     sketch.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for(let i = 0; i < size * size; i++) {
@@ -9,8 +10,9 @@ function createGrid(size) {
         div.classList.add("grid-item");
         sketch.appendChild(div);
     }
+    addHoverEffect();
 }
-
+// A function that adds a hover effect 
 function addHoverEffect() {
     let gridItem = document.querySelectorAll(".grid-item");
     gridItem.forEach(item => {
@@ -20,3 +22,18 @@ function addHoverEffect() {
     });
 }
 
+let reset = document.createElement("button");
+reset.textContent = "Reset";
+reset.style.cssText = "margin-top: 20px; font-size: 20px; letter-spacing:1px; padding: 6px; border-radius: 20px; width: 100px; border:0px; background-color: #5c5470; font-family: 'Bungee Spice'; cursor: pointer;"
+container.appendChild(reset);
+reset.addEventListener("click", () => {
+    let size = prompt("Enter new grid size:");
+    size = parseInt(size);
+    if (size && size > 0) {
+        createGrid(size);
+    } else {
+        alert("Invalid size");
+    }
+});
+
+createGrid(16);
